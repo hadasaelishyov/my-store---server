2a4f1aa3aafaca7912c9e57deab022723f3947d1
hadasaelishyov <h0548449206@gmail.com>
2025-05-13 14:20:44 +0300

    second push

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/products")
 public class AdminController {
 
     @Autowired
